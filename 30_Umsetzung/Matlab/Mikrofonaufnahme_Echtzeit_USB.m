2919e3f624bf578ae4db04689b7e347ad8efca6b
TomKreye <tom.kreye1@gmail.com>
2025-10-30 11:15:39 +0100

    Create Mikrofonaufnahme_Echtzeit_USB.m
    Matlab-Code für Aufnahme in Echtzeit des Mikrofonsignals

---
+++ 30_Umsetzung/Matlab/Mikrofonaufnahme_Echtzeit_USB.m
@@ -0,0 +1,65 @@
+% ======================================================
+% NF_Echtzeit_upsample: Sprache in Echtzeit aufnehmen,
+% auf 250 kHz hochrechnen und blockweise an Arduino Due senden
+% ======================================================
+
+%% Konfiguration
+fs = 25000;             % Eingangs-Samplingrate (Mikrofon) in Hz
+fsDAC = 250000;         % Ziel-Samplingrate (DAC) in Hz
+blockSize = 64;        % Samples pro Block vom Mikrofon
+baudRate = 2000000;     
+comPort = "COM7";       % Native USB Port des Arduino Due
+
+%% Serielle Verbindung zum Arduino
+try
+    arduinoPort = serialport(comPort, baudRate);
+    flush(arduinoPort);  % Puffer leeren
+catch
+    error("Kann den COM-Port nicht öffnen. Prüfe Verbindung und Native USB Port.");
+end
+
+%% Audio-Interface konfigurieren
+deviceReader = audioDeviceReader( ...
+    'SampleRate', fs, ...
+    'SamplesPerFrame', blockSize, ...
+    'BitDepth', '16-bit integer', ...
+    'OutputDataType', 'double');  % double für einfache Skalierung
+
+disp('Sprich jetzt ins Mikrofon...');
+
+%% Upsampling-Faktor berechnen
+upsampleFactor = round(fsDAC / fs);  % 250 kHz / 25 kHz = 10
+
+%% Live-Plot vorbereiten
+hPlot = plot(nan(blockSize*upsampleFactor,1)); % Platz für Upsampled Block
+ylim([-1 1]);
+xlabel('Sample');
+ylabel('Amplitude');
+title('Upsampled Audio (250 kHz)');
+grid on;
+drawnow;
+
+%% Ringpuffer vorbereiten (optional)
+audioBuffer = [];
+
+%% Hauptschleife
+while true
+    % 1) Block direkt vom Mikrofon
+    audioBlock = deviceReader();  % blockSize Samples bei 25 kHz
+
+    % 2) Auf 250 kHz hochrechnen
+    audioUp = resample(audioBlock, upsampleFactor, 1);  % FIR-basiert
+
+    % 3) Live-Plot aktualisieren
+    set(hPlot, 'YData', audioUp);
+    drawnow limitrate;
+
+    % 4) Signal auf 12-Bit skalieren (0-4095)
+    audio12bit = uint16((audioUp + 1) * 2047.5);
+
+    % 5) In Bytes konvertieren (Little Endian)
+    byteBlock = reshape(typecast(audio12bit, 'uint8'), [], 1);
+
+    % 7) Sofort an Arduino senden
+    write(arduinoPort, byteBlock, 'uint8');
+end
